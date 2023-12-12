be7f55768f098f088f792eda592c4fbe5a38ca6f
JonasRocha189 <jonas.rocha189@gmail.com>
2023-12-12 15:13:15 -0300

    server configuration

--- server.py
+++ server.py
@@ -8,6 +8,34 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST, PORT))
 server.listen()
 
+rooms = {}
+
+def broadcast(room, message):
+  for i in rooms[room]:
+    if isinstance(message, str):
+      message = message.encode()
+      i.send(message)
+      # print(i)
+
+def sendMessage(name, room, client):
+  while True:
+    message = client.recv(1024)
+    message - f'{name}: {message.decode()} \n'
+    broadcast(room, message)
+
 while True:
   client, addr = server.accept()
-  print(client)
+  # print(client)
+
+  client.send(b'ROOM')
+  room = client.recv(1024).decode()
+  name = client.recv(1024).decode()
+  # print(room)
+
+  if room not in rooms.keys():
+    rooms[room] = []
+  rooms[room].append(client)
+  print(f'{name} has connected in the room {room}! INFO {addr}')
+  broadcast(room, f'{name} entered the room \n')
+  thread = threading.Thread(target = sendMessage, args=(name, room, client))
+  thread.start()
--- test.py
+++ test.py
@@ -5,3 +5,8 @@ PORT = 55555
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((HOST, PORT))
+
+message = client.recv(1024)
+if message == b"ROOM":
+  client.send(b'Games')
+  client.send(b'Jonas')
