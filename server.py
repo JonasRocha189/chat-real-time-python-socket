6f9a060927bc2dffa97c61dea46207ad09f91588
JonasRocha189 <jonas.rocha189@gmail.com>
2023-12-12 12:14:11 -0300

    first socket

---
+++ server.py
@@ -0,0 +1,13 @@
+import socket
+import threading
+
+HOST = 'localhost'
+PORT = 55555
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+server.listen()
+
+while True:
+  client, addr = server.accept()
+  print(client)
